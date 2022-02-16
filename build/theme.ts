d172bdaf81a394ba634fbd7aeec30fe78a99ab2f
Sean Sullivan <me@snen.dev>
2022-02-16 12:57:30 -0500

    update theme preprocessor

--- build/theme.ts
+++ build/theme.ts
@@ -1,46 +1,66 @@
-import palettes from  "../palette.json" assert { type: "json" }
+import basePalette from  "../palette.json" assert { type: "json" }
 
-const makeLayerCss = (name: string, depth: number, color: string) =>
-`.bg-${name}-${depth} {
-  background-color: #${color}
+const makeLayerCss = (
+  feature: ColorFeature,
+  mode: 'dark' | 'light',
+  depth: number,
+  color: string,
+) => `.bg-${feature}-${depth}.${mode} {
+  background-color: #${color};
 }
 
-.border-${name}-${depth} {
-  border-color: #${color}
+.border-${feature}-${depth}.${mode} {
+  border-color: #${color};
+}
+
+.shadow-${feature}-${depth}.${mode} {
+  box-shadow: 0 0 0 1px #${color};
+}
+
+.bg-${feature}-contrast-${depth}.${mode === 'dark' ? 'light' : 'dark'} {
+  background-color: #${color};
+}
+
+.border-${feature}-contrast-${depth}.${mode === 'dark' ? 'light' : 'dark'} {
+  border-color: #${color};
+}
+
+.shadow-${feature}-contrast-${depth}.${mode === 'dark' ? 'light' : 'dark'} {
+  box-shadow: 0 0 0 1px #${color};
 }
 `
 
-// with a depth of six, each palette is 6 dark shades
-// and 6 light shades
-const MAX_DEPTH = 6
-type Twelve<T> = [T, T, T, T, T, T, T, T, T, T, T, T]
-
 // Twelve swatches are needed per color feature:
 type ColorFeature =
+  | 'grey'
   | 'primary'
   | 'accent'
   | 'surface'
   | 'warning'
-  | 'grey'
+
 interface Swatch {
   name: string
   color: string
 }
+type SwatchPalette = [Swatch, Swatch, Swatch, Swatch, Swatch, Swatch]
+
 type PaletteSpecification = {
-  color: ColorFeature
-  swatches: Twelve<Swatch>
+  color: ColorFeature,
+  contrast: 'dark' | 'light'
+  swatches: SwatchPalette
 }[]
 
-function parsePalette() {
-  
+function parsePalette(): PaletteSpecification {
+  return basePalette.map(({ paletteName, swatches }) => {
+    const [color, contrast] = paletteName.split('-') as [ColorFeature, 'dark' | 'light']
+    return { color, contrast, swatches: swatches as SwatchPalette }
+  })
 }
 
-const pal = palettes
-
-function buildPaletteCSS(): string {
-  return palettes.map(({ paletteName, swatches }) =>
+function buildPaletteCSS(palettes: PaletteSpecification): string {
+  return palettes.map(({ color: colorName, contrast, swatches }) =>
     swatches.map(
-      ({color}, i) => makeLayerCss(paletteName, i, color)
+      ({color}, i) => makeLayerCss(colorName, contrast, i, color)
     ).join('\n')
   ).join('\n')
 }
@@ -50,10 +70,13 @@ const makeSpacingCss = (depth: number, padding: number) =>
   padding: ${padding}em;
 }
 
+.block-${depth} + .block-${depth} {
+  padding-top: 0;
+}
+
 .block-${depth} > *:first-child {
   margin-top: 0;
 }
-
 .block-${depth} > *:last-child {
   margin-bottom: 0;
 }
@@ -63,28 +86,33 @@ const makeSpacingCss = (depth: number, padding: number) =>
 }
 `
 
-function buildSpacingCSS(): string {
+function buildSpacingCSS(palettes: PaletteSpecification): string {
   const maxDepth = palettes[0].swatches.length
   return Array(maxDepth).fill(0).map((_, index) =>
     makeSpacingCss(index, 2.5 - 2.75 * index / maxDepth)
   ).join('\n')
 }
 
-const makeTextColorCSS = (color: string) =>
-`.text-color {
-  color: #${color}
+const makeTextColorCSS = (lightModeColor: string, darkModeColor: string) =>
+`.text-color.light{
+  color: #${lightModeColor};
+}
+
+.text-color.dark {
+  color: #${darkModeColor};
 }
 `
 
-function buildTextColorCSS(): string {
-  const contrastSwatches = palettes.find((palette) => palette.paletteName === "contrast")?.swatches
-  const textColor = contrastSwatches?.[contrastSwatches.length - 1]?.color
-  return textColor ? makeTextColorCSS(textColor) : ''
+function buildTextColorCSS(palettes: PaletteSpecification): string {
+  const blackTextColor = palettes.find(({ color, contrast}) => color === "grey" && contrast === "dark")?.swatches[0]?.color
+  const whiteTextColor = palettes.find(({ color, contrast}) => color === "grey" && contrast === "light")?.swatches[0]?.color
+  return blackTextColor && whiteTextColor ? makeTextColorCSS(blackTextColor, whiteTextColor) : ''
 }
 
 export default function buildCSS(): string {
-  const paletteCss = buildPaletteCSS()
-  const spacingCss = buildSpacingCSS()
-  const textCss = buildTextColorCSS()
+  const palette = parsePalette()
+  const paletteCss = buildPaletteCSS(palette)
+  const spacingCss = buildSpacingCSS(palette)
+  const textCss = buildTextColorCSS(palette)
   return [paletteCss, spacingCss, textCss].join('\n')
 }
