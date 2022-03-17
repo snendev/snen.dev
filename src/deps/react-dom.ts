b22844f400ed61361c827a0c2e07b149af8502c9
Sean Sullivan <me@snen.dev>
2022-03-17 17:10:50 -0400

    update deps

--- src/deps/react-dom.ts
+++ src/deps/react-dom.ts
@@ -1,3 +1,3 @@
-import ReactDOM from "https://cdn.esm.sh/react-dom@18.0.0-rc.0?deps=react@18.0.0-rc.0&pin=v66";
+import ReactDOM from "https://cdn.esm.sh/react-dom@18.0.0-rc.0?deps=react@18.0.0-rc.0&pin=v69";
 
 export default ReactDOM;
--- src/deps/react.ts
+++ src/deps/react.ts
@@ -1,3 +1,3 @@
-import React from "https://cdn.esm.sh/react@18.0.0-rc.0?pin=v66";
+import React from "https://cdn.esm.sh/react@18.0.0-rc.0?pin=v69";
 
 export default React;
