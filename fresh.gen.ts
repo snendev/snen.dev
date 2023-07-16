45c3f7ea3ceb8deca7a8a4d7214d38891802c165
Sean Sullivan <sullivansean27@gmail.com>
2023-07-16 14:36:23 -0400

    remove counter island

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,15 +4,12 @@
 
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
     "./routes/index.tsx": $0,
   },
-  islands: {
-    "./islands/Counter.tsx": $$0,
-  },
+  islands: {},
   baseUrl: import.meta.url,
   config,
 };
